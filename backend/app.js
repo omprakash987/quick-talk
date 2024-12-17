775d094e265f30e8dd5e417f7e794e8dc81b97d3
omprakash parewa <kanuparewa371@gmail.com>
2024-12-17 17:36:34 +0530

    app.js update

--- backend/app.js
+++ backend/app.js
@@ -7,9 +7,8 @@ import connectMongoDB from './db/connectMongoDB.js';
 import userRoutes from './routes/user.route.js';
 import cookieParser from 'cookie-parser';
 import notifications from './routes/notification.route.js'
-import path from 'path'
-
 import postRoutes from './routes/post.route.js'
+import path from 'path'; 
 
 dotenv.config(); 
 console.log("cloudname : ", process.env.CLOUDINARY_CLOUD_NAME)
@@ -18,11 +17,13 @@ cloudinary.config({
     cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
     api_key:process.env.CLOUDINARY_API_KEY,
     api_secret:process.env.CLOUDINARY_API_SECRET,
-}); 
+});
 
 const app = express(); 
-const PORT = process.env.PORT || 8000; 
 const __dirname = path.resolve(); 
+
+const PORT = process.env.PORT|| 8000; 
+ 
 
 app.use(express.json({limit:"5mb"})); 
 app.use(express.urlencoded({extended:true})); 
@@ -32,8 +33,9 @@ app.use('/api/users',userRoutes);
 app.use('/api/posts',postRoutes); 
 app.use('/api/notifications',notifications)
 
-if(process.env.NODE_ENV === "production"){
+if(process.env.NODE_ENV == "production"){
     app.use(express.static(path.join(__dirname,"/frontend/dist"))); 
+
     app.get("*",(req,res)=>{
         res.sendFile(path.resolve(__dirname,"frontend","dist","index.html")); 
     })
